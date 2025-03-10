c827cf94dd7bc59289f45521630f3010c10101cc
Logis-Mac <>
2025-03-09 23:04:50 -0700

    - Android Studio 업그레이드로 외부 라이브러리 변경. ios 소스변경

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -2,71 +2,69 @@ import UIKit
 import Flutter
 import Firebase
 import TMapSDK
-
-@UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate, TMapTapiDelegate {
-  let appKey:String = "l7xx9363c407318b4b04910193a57d19242a";
-
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-  
-    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
-    let channel = FlutterMethodChannel(name: "logis.flutter.iostmap",binaryMessenger: controller.binaryMessenger)
-
-    channel.setMethodCallHandler({
-      [weak self] (call: FlutterMethodCall, result: FlutterResult) -> Void in
-
-      switch (call.method) {
-      case "initTmapAPI":
-        self?.initTmapAPI()
-        result("initTmapAPI")
-        break;
-      case "isTmapApplicationInstalled":
-        if(TMapApi.isTmapApplicationInstalled()) {
-          result("")
-        }else{
-          let url = TMapApi.getTMapDownUrl()
-          result(url)
+@main
+@objc class AppDelegate: FlutterAppDelegate, TMapTapiDelegate{
+    let appKey: String = "l7xx9363c407318b4b04910193a57d19242a"
+    
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
+        let channel = FlutterMethodChannel(name: "logis.flutter.iostmap", binaryMessenger: controller.binaryMessenger)
+        TMapApi.setSKTMapAuthenticationWithDelegate(self, apiKey: appKey)
+        channel.setMethodCallHandler { [weak self] (call: FlutterMethodCall, result: @escaping FlutterResult) in
+            guard let self = self else { return }
+            
+            switch call.method {
+            case "showActivity":
+                if let args = call.arguments as? [String: Any],
+                let name = args["name"] as? String,
+                let latitude = args["lat"] as? Double,
+                   let longitude = args["lon"] as? Double {
+                    
+                    let urlStr = "tmap://route?rGoName=\(name)&rGoX=\(longitude)&rGoY=\(latitude)"
+                    
+                    guard let encodedStr = urlStr.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {return}
+                    guard let url = URL(string:encodedStr) else {return}
+                    
+                    guard let appStoreURL = URL(string: "http://itunes.apple.com/app/id431589174") else {return}
+                    
+                    if(UIApplication.shared.canOpenURL(url)) {
+                        UIApplication.shared.open(url)
+                    } else {
+                        UIApplication.shared.open(appStoreURL)
+                    }
+                }
+            default:
+                result(FlutterMethodNotImplemented)
+            }
         }
-        break;  
-      default:
-        result(FlutterMethodNotImplemented)
-        break;
+
+            SwiftFlutterForegroundTaskPlugin.setPluginRegistrantCallback { registry in
+            GeneratedPluginRegistrant.register(with: registry)
+        }
         
-      }
-    })
-
-    SwiftFlutterForegroundTaskPlugin.setPluginRegistrantCallback(registerPlugins)
-     if #available(iOS 10.0, *) {
-      // For iOS 10 display notification (sent via APNS)
-      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
-
-      let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-      UNUserNotificationCenter.current().requestAuthorization(
-        options: authOptions,
-        completionHandler: { _, _ in }
-      )
-    } else {
-      let settings: UIUserNotificationSettings =
-        UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
-      application.registerUserNotificationSettings(settings)
+        if #available(iOS 10.0, *) {
+            UNUserNotificationCenter.current().delegate = self
+            
+            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+            UNUserNotificationCenter.current().requestAuthorization(options: authOptions) { _, _ in }
+            UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+        } else {
+            let settings = UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+            application.registerUserNotificationSettings(settings)
+        }
+        application.registerForRemoteNotifications()
+        
+        FirebaseApp.configure()
+        GeneratedPluginRegistrant.register(with: self)
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
-       application.registerForRemoteNotifications()
-
-    FirebaseApp.configure()
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-
-  private func initTmapAPI() {
-    TMapApi.setSKTMapAuthenticationWithDelegate(self,apiKey: appKey)
-  }
 
 }
 
 
-func registerPlugins(registry: FlutterPluginRegistry) {
-    GeneratedPluginRegistrant.register(with: registry)
-}
+    func registerPlugins(registry: FlutterPluginRegistry) {
+        GeneratedPluginRegistrant.register(with: registry)
+    }
